303ff1cae7ad4931794d83cb23faa9658d3f29a6
AlfonsoDiezRamirez <alfonso.diez.ramirez@alumnos.upm.es>
2023-05-27 15:38:32 +0200

    prueba con elasticsearch, funciona pero no inserta en el indice

--- beatstologstashProvision.sh
+++ beatstologstashProvision.sh
@@ -1,8 +1,10 @@
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
-sudo apt-get update && sudo apt-get install filebeat
+sudo apt-get update && sudo apt-get install filebeat=8.5.3
 sudo cp /sharedData/filebeat.yml /etc/filebeat/filebeat.yml
+pass=$(cat /sharedData/admin)
+sudo sed -i "s/#PASSWORD#/$pass/g" /etc/filebeat/filebeat.yml
 sudo systemctl daemon-reload
 sudo systemctl enable filebeat
 sudo systemctl start filebeat
