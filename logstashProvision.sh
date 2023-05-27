e0eb3da18e65b1a5ab9a628241927b0468d02c39
AlfonsoDiezRamirez <alfonso.diez.ramirez@alumnos.upm.es>
2023-05-27 17:56:06 +0200

    funcionando toda la parte técnica

--- logstashProvision.sh
+++ logstashProvision.sh
@@ -2,7 +2,7 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt-get update && sudo apt-get install logstash=1:8.5.3-1 -y
-#instalamos el plugin de mqtt:
+
 pass=$(cat /sharedData/admin)
 sudo cp /sharedData/pipelines.yml /etc/logstash/pipelines.yml
 sudo cp /sharedData/beatstoelastic.config /etc/logstash/beatstoelastic.config
@@ -11,5 +11,6 @@ sudo chown -R logstash.logstash /usr/share/logstash
 sudo chmod 777 /usr/share/logstash/data
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable logstash.service
-sudo /usr/share/logstash/bin/logstash-plugin install logstash-input-paho-mqtt
+#instalamos el plugin de mqtt:
+#sudo /usr/share/logstash/bin/logstash-plugin install logstash-input-paho-mqtt
 sudo systemctl start logstash.service
