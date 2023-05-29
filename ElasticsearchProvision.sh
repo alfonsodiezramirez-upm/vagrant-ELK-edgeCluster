cfd7c7609a24d286b8c227ddeed8b87d9a342321
AlfonsoDiezRamirez <alfonso.diez.ramirez@alumnos.upm.es>
2023-05-29 20:10:24 +0200

    scripts sin cosas feas y memoria con código

--- ElasticsearchProvision.sh
+++ ElasticsearchProvision.sh
@@ -1,10 +1,3 @@
-#wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.5.2-linux-x86_64.tar.gz
-#wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.5.2-linux-x86_64.tar.gz.sha512
-#shasum -a 512 -c elasticsearch-8.5.2-linux-x86_64.tar.gz.sha512 
-#tar -xzf elasticsearch-8.5.2-linux-x86_64.tar.gz
-#mv elasticsearch-8.5.2 elasticsearch
-#cd elasticsearch/
-#yes |./bin/elasticsearch -d -p pid
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jre
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
@@ -18,7 +11,7 @@ fullPass=$(yes | sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password 
 pass=${fullPass: -20}
 #Utilizamos para albergar la contraseña y el token un fichero en una synced folder para poder usarlo en otra Maquina virtual.
 echo $pass>/sharedData/admin
-#Es importante no ejecutar este comando con root dado que si ejecutamos la creación del token como tal, el fichero service_tokens no cuenta con permisos de lectura para el usuario elasticsearch y por tanto no levanta el servicio, como alternativa si se muestra el error /etc/default/elasticsearch: permission denied, podemos ejecutarlo como root y cambiar los permisos de service_tokens más adelante.
+#Es importante no ejecutar este comando como root dado que si ejecutamos la creación del token como tal, el fichero service_tokens no cuenta con permisos de lectura para el usuario elasticsearch y por tanto no levanta el servicio, como alternativa si se muestra el error /etc/default/elasticsearch: permission denied, podemos ejecutarlo como root y cambiar los permisos de service_tokens más adelante.
 fullToken=$(sudo /usr/share/elasticsearch/bin/elasticsearch-service-tokens create elastic/kibana kibana)
 token=${fullToken: -64}
 echo $token>/sharedData/token
--- logstashProvision.sh
+++ logstashProvision.sh
@@ -2,7 +2,6 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt-get update && sudo apt-get install logstash=1:8.5.3-1 -y
-
 pass=$(cat /sharedData/admin)
 sudo cp /sharedData/pipelines.yml /etc/logstash/pipelines.yml
 sudo cp /sharedData/beatstoelastic.config /etc/logstash/beatstoelastic.config
@@ -11,6 +10,6 @@ sudo chown -R logstash.logstash /usr/share/logstash
 sudo chmod 777 /usr/share/logstash/data
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable logstash.service
-#instalamos el plugin de mqtt:
+#instalamos el plugin de mqtt, se descarta por decisión de diseño
 #sudo /usr/share/logstash/bin/logstash-plugin install logstash-input-paho-mqtt
 sudo systemctl start logstash.service
